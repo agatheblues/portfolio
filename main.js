fbca1342403c5fb7c896a4a737a4d258fae131b8
agatheblues <agatheblues@users.noreply.github.com>
2017-09-16 17:36:20 +0200

    Renaming app.js to main.js

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import {Gallery} from './components/Gallery/Gallery.js';
 require('./main.scss');
 
+// Cache gallery container
+const galleryContainer = document.querySelector('.gallery-container');
+
+// Create new array with URLs for images
+let imgUrlsList = [
+  'https://source.unsplash.com/3Z70SDuYs5g/800x600',
+  'https://source.unsplash.com/01vFmYAOqQ0/800x600',
+  'https://source.unsplash.com/2Bjq3A7rGn4/800x600',
+  'https://source.unsplash.com/t20pc32VbrU/800x600',
+  'https://source.unsplash.com/pHANr-CpbYM/800x600',
+  'https://source.unsplash.com/3PmwYw2uErY/800x600',
+  'https://source.unsplash.com/uOi3lg8fGl4/800x600',
+  'https://source.unsplash.com/CwkiN6_qpDI/800x600',
+  'https://source.unsplash.com/9O1oQ9SzQZQ/800x600',
+  'https://source.unsplash.com/E4944K_4SvI/800x600',
+  'https://source.unsplash.com/-hI5dX2ObAs/800x600',
+  'https://source.unsplash.com/vZlTg_McCDo/800x600'
+];
+
 ReactDOM.render(
   <Gallery imgUrls={imgUrlsList} />,
   galleryContainer
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: ['./app.js'],
+  entry: ['./main.js'],
   output: {
     filename: 'bundle.js'
   },
