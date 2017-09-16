529b932e91e2bc868b8ac83c2edaac8c96ac99e3
agatheblues <agatheblues@users.noreply.github.com>
2017-09-16 17:31:32 +0200

    Gallery with sass compiling

--- webpack.config.js
+++ webpack.config.js
@@ -27,17 +27,15 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          //resolve-url-loader may be chained before sass-loader if necessary
           use: ['css-loader', 'sass-loader']
         })
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css'),
+    new ExtractTextPlugin('style.css'),
     new webpack.HotModuleReplacementPlugin()
   ]
 };
