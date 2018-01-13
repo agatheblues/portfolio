de31c9adde93f76f71b2f955602f60e657dcb51b
agatheblues <agatheblues@users.noreply.github.com>
2018-01-13 20:54:15 +0100

    "Bjork processing project, first files and attempt

---
+++ components/Project/ImageMappingProcessing/ImageMappingProcessing.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import * as processing from 'processing-js';
+require('../ImageMappingProcessing/ImageMappingProcessing.scss');
+var createReactClass = require('create-react-class');
+
+
+const ImageMappingProcessing = createReactClass({
+  propTypes: {
+    projectData: PropTypes.object.isRequired
+  },
+  componentDidMount() {
+  },
+  render() {
+    return (
+      <div>
+        <div className='ImageMappingProcessing-title'>
+          <h2>{this.props.projectData.title}</h2>
+        </div>
+        <div className='ImageMappingProcessing-description'>
+          <p>{this.props.projectData.longDescription}</p>
+        </div>
+        <canvas data-processing-sources="pixel_to_the_line.pde"></canvas>
+      </div>
+    );
+  }
+});
+
+export {ImageMappingProcessing};
--- components/Project/Project.js
+++ components/Project/Project.js
@@ -1,6 +1,7 @@
 require('../Project/Project.scss');
 import {Koala} from './Koala/Koala.js';
 import {DotsStraightLine} from './DotsStraightLine/DotsStraightLine.js';
+import {ImageMappingProcessing} from './ImageMappingProcessing/ImageMappingProcessing.js';
 import PropTypes from 'prop-types';
 import {Header} from '../Header/Header.js';
 import React from 'react';
@@ -18,6 +19,9 @@ const Project = createReactClass({
       break;
     case 'project-dots-straight-line':
       return(<DotsStraightLine projectData={this.props.cardItem}/>);
+      break;
+    case 'project-image-mapping-processing':
+      return(<ImageMappingProcessing projectData={this.props.cardItem}/>);
       break;
     default:
       return false;
