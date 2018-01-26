4765102d13e36c032fbe0918752a0239faa9c5ba
agatheblues <agatheblues@users.noreply.github.com>
2018-01-26 21:18:15 +0100

    Add p5 package

--- components/Project/ImageMappingProcessing/ImageMappingProcessing.js
+++ components/Project/ImageMappingProcessing/ImageMappingProcessing.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import * as processing from 'processing-js';
 require('../ImageMappingProcessing/ImageMappingProcessing.scss');
 var createReactClass = require('create-react-class');
 
